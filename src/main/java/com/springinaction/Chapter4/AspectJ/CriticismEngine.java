b09ebe3a17541cca801e2fe8c20af6015d4cfd81
zhulifang <zlf1227@163.com>
2018-08-08 18:00:40 +0800

    add example for chapter 4.5 --AspectJ

---
+++ src/main/java/com/springinaction/Chapter4/AspectJ/CriticismEngine.java
@@ -0,0 +1,5 @@
+package com.springinaction.Chapter4.AspectJ;
+
+public interface CriticismEngine {
+    public void getCriticism();
+}
---
+++ src/main/java/com/springinaction/Chapter4/AspectJ/JZhouConcert.java
@@ -0,0 +1,10 @@
+package com.springinaction.Chapter4.AspectJ;
+
+
+
+public class JZhouConcert implements Performance {
+
+    public void perform() {
+        System.out.println("Jay Zhou's concert begins !");
+    }
+}
